a2469279e95789aedd241bd9561333efc791f703
LeeSuWan626 <75827037+LeeSuWan626@users.noreply.github.com>
2020-12-15 14:46:16 +0900

    image file
    Train, Test 나눠져 있음

--- Url03.py
+++ Url03.py
@@ -13,13 +13,13 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 
 
-'''
+
 !if not exist "./files" mkdir files
 # Download Face detection XML 
 !curl -L -o ./files/haarcascade_frontalface_default.xml https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
 # Download emotion trained data
 !curl -L -o ./files/emotion_model.hdf5 https://mechasolution.vn/source/blog/AI-tutorial/Emotion_Recognition/emotion_model.hdf5
-'''
+
 
 
 # Face detection XML load and trained model loading
