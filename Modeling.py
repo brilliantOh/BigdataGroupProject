a8b15b843b053b1a4528c8000a1912c02393cde5
Soo93 <75824040+Soo93@users.noreply.github.com>
2020-12-15 21:36:37 +0900

    Update Modeling.py

--- Modeling.py
+++ Modeling.py
@@ -2,25 +2,33 @@
 """
 Modeling.py
 
-@author: Soo
+1. Transfer Learning
+ - 
+2. Fine Tuning
+
+
 """
 
 from tensorflow.keras.models import load_model
-from skimage import io
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Flatten
-from tensorflow.python.client import device_lib
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten 
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Dropout, Flatten 
 from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from skimage import io
-import matplotlib.pyplot as plt
+
+
+
 
 # 기존 모델 로딩
 model_original = load_model('C:/ITWILL/AI_Interview_App/Scripts/files/emotion_model.hdf5', compile=False)
 
 model_original.summary()
+
+
+# Model 생성
+model = Sequential()
+
+input_shape = (48, 48, 1)
+
+model.add(Conv2D(50, kernelsize))
 
 
 
